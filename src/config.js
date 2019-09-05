cb00240ff396b6bd6dc483f0a4a0301a70d8f297
Chris Wysocki <christopher.wysocki@gmail.com>
2019-09-05 12:30:01 -0700

    added a secure login

--- src/config.js
+++ src/config.js
@@ -1,5 +1,8 @@
 module.exports = {
-  PORT: process.env.PORT || 8000,
-  NODE_ENV: process.env.NODE_ENV || 'development',
-  DB_URL: process.env.DB_URL || 'postgresql://thingful@localhost/thingful',
-}
+	PORT: process.env.PORT || 8000,
+	NODE_ENV: process.env.NODE_ENV || 'development',
+	DB_URL: process.env.DB_URL || 'postgresql://thingful@localhost/thingful',
+	JWT_SECRET:
+		process.env.JWT_SECRET ||
+		'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoxLCJpYXQiOjE1NTA5NDQ5NDMsInN1YiI6InRlc3QtdXNlci0xIn0.OQTyYphJrG0lXmh7okySbmlFDgP11mSzlDoyJg7ERs0'
+};
